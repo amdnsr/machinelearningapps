9de33711ee9e6b5ef3e4504de78ad607803f515a
Ahmad Nasir <33130717+amdnsr@users.noreply.github.com>
2021-09-02 15:03:50 +0530

    created a default user, added 3 tasks and added commands for the new databse schema, and updated the database according to the schema.

--- project/database_commands.sql
+++ project/database_commands.sql
@@ -13,11 +13,32 @@
 -- DELETE FROM profile;
 -- DELETE FROM jobs;
 
+-- DROP TABLE jobs;
+-- DROP TABLE profile;
+-- DROP TABLE users;
 
 -- CREATE TABLE 'users' ('id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'username' text NOT NULL UNIQUE, 'email' text NOT NULL UNIQUE, 'hash' text NOT NULL, 'time' text NOT NULL);
 -- CREATE TABLE 'profile' ('id' integer PRIMARY KEY NOT NULL, 'fname' text NOT NULL, 'lname' text NOT NULL);
 -- CREATE TABLE 'jobs' ('job_id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'user_id' integer NOT NULL, 'file_count' integer NOT NULL, 'time' text NOT NULL);
 
+
+-- CREATE TABLE 'users' ('id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'username' text NOT NULL UNIQUE, 'email' text NOT NULL UNIQUE, 'hash' text NOT NULL, 'time' text NOT NULL);
+-- CREATE TABLE 'profile' ('id' integer PRIMARY KEY NOT NULL REFERENCES users(id), 'fname' text NOT NULL, 'lname' text NOT NULL);
+-- CREATE TABLE 'jobs' ('job_id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'user_id' integer NOT NULL REFERENCES users(id), 'task_type' text NOT NULL, 'file_count' integer NOT NULL, 'status' text DEFAULT ('NA'), 'time' text NOT NULL, 'completion_time' text NOT NULL);
+
+-- CREATE TABLE 'users' ('id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'username' text NOT NULL UNIQUE, 'email' text NOT NULL UNIQUE, 'hash' text NOT NULL, 'time' text NOT NULL);
+-- CREATE TABLE 'profile' ('id' integer PRIMARY KEY NOT NULL REFERENCES users(id), 'fname' text NOT NULL, 'lname' text NOT NULL);
+-- CREATE TABLE 'jobs' ('job_id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'user_id' integer NOT NULL REFERENCES users(id), 'task_type' text NOT NULL, 'file_count' integer NOT NULL, 'status' text DEFAULT ('NA'), 'time' text NOT NULL, 'completion_time' text NOT NULL);
+
+DROP TABLE jobs;
+DROP TABLE profile;
+DROP TABLE users;
+
+CREATE TABLE 'users' ('id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'username' text NOT NULL UNIQUE, 'email' text NOT NULL UNIQUE, 'hash' text NOT NULL, 'time' text NOT NULL);
+CREATE TABLE 'profile' ('id' integer PRIMARY KEY NOT NULL REFERENCES users(id), 'fname' text NOT NULL, 'lname' text NOT NULL);
+CREATE TABLE 'jobs' ('job_id' integer PRIMARY KEY AUTOINCREMENT NOT NULL, 'user_id' integer NOT NULL REFERENCES users(id), 'task_type' text NOT NULL, 'file_count' integer NOT NULL, 'status' text DEFAULT ('NA'), 'creation_time' text NOT NULL, 'completion_time' text NOT NULL);
+
+.quit
 
 -- DROP TABLE jobs;
 -- DROP TABLE profile;
